7731afd2eeca9c7e73e6e6380f89562df0a119b4
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2023-12-19 23:05:21 -0300

    Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,20 @@
+# numero1 = input("Digite um número:") #Recebe o primeiro valor
+print("Digite a operação desejada:")
+print("+ para somar")
+print("- para subtrair")
+print("* para multiplicar")
+print("/ para dividir")
+operador = input("")
+numero2 = input("Digite um número:") #Recebe o segundo valor
+numero1=float(numero1)
+numero2=float(numero2)
+if "+" in operador: #Soma os dois operadores
+    numero1 = numero1 + numero2
+if "-" in operador: #Subtrai os dois operadores
+    numero1 = numero1 - numero2
+if "*" in operador: #multiplica os dois operadores
+    numero1 = numero1 * numero2
+if "/" in operador: #Divide os dois operadores1
+    numero1 = numero1 / numero2
+
+print (numero1)
