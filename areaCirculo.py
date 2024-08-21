9763c24f7babfabe7000f65f7d49ccc5824a3c9a
Arthur de Sousa Santana <128152061+Arthurito37@users.noreply.github.com>
2024-08-20 21:04:22 -0300

    Add files via upload

---
+++ areaCirculo.py
@@ -0,0 +1,7 @@
+pi = 3.14
+i = [0] * 2 #cria array que armazena os valores
+i [0] = float(input("Digite o raio do círculo"))
+
+i [1] = pi * (i [0] * i [0]) #faz o calculo da área do círculo
+
+print(f"O valor da área do círculo de raio {i[0]} equivale a {i[1]}")
